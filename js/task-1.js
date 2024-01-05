267ad00a660e8f8c6de2011959ca71b5dcfb6e17
OlhaArtemenko <global14072012@gmail.com>
2024-01-05 12:19:54 +0200

    home work

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,17 @@
-function slugify(title) {
-  const titleToLower = title.toLowerCase();
-  const titleToArray = titleToLower.split(' ');
-  const slug = titleToArray.join('-');
-  return slug;
+function isEnoughCapacity(products, containerSize) {
+  let totalAmountOfProducts = 0;
+  let arrayProdValue = Object.values(products);
+
+  for (let prod of arrayProdValue) {
+    totalAmountOfProducts += prod;
+  }
+  return totalAmountOfProducts <= containerSize;
 }
-console.log(slugify('Arrays for begginers')); // "arrays-for-begginers"
-console.log(slugify('English for developer')); // "english-for-developer"
-console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
-console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
